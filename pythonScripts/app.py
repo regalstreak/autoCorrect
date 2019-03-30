ea4d45f7b8bdcbe2fb1eb3b88efa04bfd621cadb
arindam-modak <arindam.modak03@gmail.com>
2019-03-30 08:18:24 +0530

    schema added

--- pythonScripts/app.py
+++ pythonScripts/app.py
@@ -21,10 +21,76 @@ app.config['SECRET_KEY'] = "random string"
 db = SQLAlchemy(app)
 
 
+class test(db.Model):
+	__tablename__ = 'test'
+	column_display_pk = True
+	testid = db.Column(db.Integer, primary_key=True)
+	testname = db.Column(db.String(40))
+	organized_by = db.Column(db.String(40))
+	testdate = db.Column(db.DateTime)
+	venue = db.Column(db.String(40))
+
+class question(db.Model):
+	__tablename__ = 'question'
+	column_display_pk = True
+	questionid = db.Column(db.Integer, primary_key=True)
+	testid = db.Column(db.Integer)
+	questionSt = db.Column(db.String(100))
+	marks = db.Column(db.Integer)
+	answerVec = db.Column(db.String(500))
+	numOfParts = db.Column(db.Integer)
+
+class answer(db.Model):
+	__tablename__ = 'answer'
+	column_display_pk = True
+	answerid = db.Column(db.Integer, primary_key=True)
+	testid = db.Column(db.Integer)
+	questionid = db.Column(db.Integer)
+	marks = db.Column(db.Integer)
+	url = db.Column(db.String(100))
+
+
 @app.route('/')
 def index():
 	result = {'hello': 'world'}
 	return make_response(jsonify(result), 201)
+
+
+from flask import Flask, url_for, send_from_directory, request
+import logging, os
+from werkzeug import secure_filename
+
+file_handler = logging.FileHandler('server.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = '{}/uploads/'.format(PROJECT_HOME)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def create_new_folder(local_dir):
+    newpath = local_dir
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    return newpath
+
+
+
+@app.route('/api/upload/', methods = ['POST'])
+def api_root():
+    app.logger.info(PROJECT_HOME)
+    if request.method == 'POST' and request.files['image']:
+    	app.logger.info(app.config['UPLOAD_FOLDER'])
+    	img = request.files['image']
+    	img_name = secure_filename(img.filename)
+    	create_new_folder(app.config['UPLOAD_FOLDER'])
+    	saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+    	app.logger.info("saving {}".format(saved_path))
+    	img.save(saved_path)
+    	return send_from_directory(app.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
+
+
 
 
 if __name__ == '__main__':
